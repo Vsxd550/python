06c78f610a76e7d8376178b65a840b57c309c955
Vsxd550 <58628491+Vsxd550@users.noreply.github.com>
2023-01-20 17:09:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 # initialize the app with the extension
 db.init_app(app)
 
-#dbオブジェクトのModelというクラスを継承する
+
 class Post(db.Model):
-    #多分dbオブジェクトのColumnクラスに引数を渡してそれぞれインスタンス化している
+   
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     body = db.Column(db.String, nullable=False)
@@ -23,28 +23,23 @@ class Post(db.Model):
 @app.route("/",methods = ["GET","POST"])
 def index():
     if request.method == "GET":
-        #Postクラスのデータをすべて取ってくる
+      
         posts = Post.query.all()
-        #新たに追加された情報を含むpostsという変数をindex.htmlに渡す
+       
         return render_template("index.html", posts = posts)
 
 @app.route("/form", methods=["GET", "POST"])
 def create():
-    #requestオブジェクトのmethodという属性がPOSTだったら、
-    #form.htmlのページから送られてきた情報の名前の属性が"title"と"body"という
-    #ものをtitle,bodyにそれぞれ代入する
     if request.method == "POST":
         title = request.form.get("title")
         body = request.form.get("body")
         
-        #postとしてインスタンス化する
+       
         post = Post(title = title, body = body)
         
-        #db.session.addでdbオブジェクトに追加して、
-        #db.session.commitで保存する
         db.session.add(post)
         db.session.commit()
-        #元のページに戻る
+
         return redirect("/")
         
     elif request.method == "GET":
